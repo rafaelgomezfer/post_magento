0f6db6bd52a65afc618079156dc2df2cbc12b178
Rafael <rafagf89@gmail.com>
2024-05-03 09:41:19 -0500

    view list

---
+++ app/code/Custom/NttBlogs/Controller/Adminhtml/Posts/Index.php
@@ -0,0 +1,47 @@
+<?php
+namespace Custom\NttBLogs\Controller\Adminhtml\Posts;
+
+class Index extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'Custom_NttBLogs::index';
+    const PAGE_TITLE = 'Posts';
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     */
+    public function __construct(
+       \Magento\Backend\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->_pageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__(static::PAGE_TITLE));
+        return $resultPage;
+    }
+
+    /**
+     * Is the user allowed to view the page.
+    *
+    * @return bool
+    */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed(static::ADMIN_RESOURCE);
+    }
+}
